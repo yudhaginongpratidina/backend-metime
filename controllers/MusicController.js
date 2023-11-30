454bdb5583df6ff1d2ad3b78e62756186f000a6e
YUDHA GINONG PRATIDINA <dev.yudhaginongpratidina@gmail.com>
2023-11-30 08:10:03 +0700

    Create Service Music

--- controllers/MusicController.js
+++ controllers/MusicController.js
@@ -1,5 +1,7 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
+const path = require('path');
+const fsPromises = require('fs').promises;
 
 const getMusics = async (req, res) => {
     try {
@@ -10,6 +12,121 @@ const getMusics = async (req, res) => {
     }
 }
 
+const getMusicById = async (req, res) => {
+    try {
+        const response = await prisma.music.findFirst({ where: { id: Number(req.params.id) } });
+        if(!response) return res.status(404).json({msg: "Music not found"});
+        return res.status(200).json({msg: "Success get music", response});
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const uploadMusic = async (req, res) => {
+    let finalMusicURL = req.protocol + '://' + req.get('host') + '/music/' + req.file.filename;
+    const { title } = req.body;
+
+    // CEK EKTENSI FILE
+    const fileName = req.file.filename;
+    const fileExtension = path.extname(fileName).toLowerCase();
+
+    // CEK DATA MUSIC
+    const fileMusicURLExists = await prisma.music.findFirst({ where: { url: finalMusicURL } })
+    if(fileMusicURLExists) return res.status(409).json({msg: "File already exists"});
+
+    // UPLOAD MUSIC
+    try {
+        const response = await prisma.music.create({ data : {
+            title: title,
+            url: finalMusicURL
+        }});
+        return res.status(201).json({msg : "File uploaded successfully", response});
+    } catch (error) {
+        return res.status(500).json({msg: error.message});
+    }
+
+}
+
+const updateMusicById = async (req, res) => {
+    try {
+
+        // TANGKAP ID, TITLE, DAN ARTIST
+        const id = Number(req.params.id);
+        const { title } = req.body;
+        
+        // CEK DATA
+        const existingMusic = await prisma.music.findFirst({ where: { id: id } });
+        if (!existingMusic) return res.status(404).json({ msg: 'Music not found' });
+
+        // TANGKAP URL MUSIC YANG DIKIRIMKAN DARI MIDLLEEWARE
+        const finalMusicURL = req.protocol + '://' + req.get('host') + '/music/' + req.file.filename;
+
+        // DAPATKAN NAMA FILE
+        const fileName = req.file.filename;
+
+        // CEK EKTENSI FILE (FILE HARUS MP3)
+        const fileExtension = path.extname(fileName).toLowerCase();
+        if (fileExtension !== '.mp3') return res.status(400).json({ msg: 'Only .mp3 files are allowed' });
+
+        // FILE EXIST
+        const fileExist = await prisma.music.findFirst({ where: { url: finalMusicURL } })
+        if (fileExist) return res.status(409).json({ msg: 'File already exists' });
+
+        // TIMPA FILE MUSIC DENGAN CARA HAPUS FILE MUSIC SEBELUMNYA
+        const oldFileName = existingMusic.url.split('/music/')[1];
+        const oldFilePath = path.join(process.cwd(), 'public', 'music', oldFileName);
+        await fsPromises.unlink(oldFilePath);
+
+        // UPDATE DATA MUSIC DI DATABASE DENGAN URL BARU BERDASARKAN ID
+        const response = await prisma.music.update({
+            where: { id: id},
+            data: {
+                title: title,
+                url: finalMusicURL
+            }
+        });
+        
+        return res.status(200).json({ msg: 'Music updated successfully', response });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ msg: 'Internal Server Error' });
+    }
+}
+
+
+const deleteMusicById = async (req, res) => {
+    try {
+
+        // AMBIL DATA MUSIC DI DATABASE BERDASARKAN ID
+        const response = await prisma.music.findFirst({ where: { id: Number (req.params.id)}});
+
+        // MUSIC TIDAK DITEMUKAN
+        if (!response) return res.status(404).json({ msg: 'Music not found' });
+
+        // GET NAMA FILE SETELAH /MUSIC/
+        const fileName = response.url.split('/music/')[1];
+
+        // SET FILE PATH
+        const filePath = path.join(process.cwd(), 'public', 'music', fileName);
+
+        // HAPUS FILE BERDASARKAN PATH YANG TELAH DIDAPAT
+        await fsPromises.unlink(filePath);
+
+        // HAPUS DATA MUSIC ATAU RECORD DI DATABASE
+        await prisma.music.delete({ where: { id: Number (req.params.id)}});
+
+        return res.status(200).json({ msg: 'Music deleted successfully', response })
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ msg: 'Internal Server Error' });
+    }
+}
+
 module.exports = {
-    getMusics
+    getMusics,
+    getMusicById,
+    uploadMusic,
+    updateMusicById,
+    deleteMusicById
 }
--- routes/MusicRoute.js
+++ routes/MusicRoute.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
+const UploadMusic = require("../middleware/music");
 const MusicController = require("../controllers/MusicController");
 
 router.get("/api/musics", MusicController.getMusics);
+router.get('/api/musics/:id', MusicController.getMusicById);
+router.post('/api/musics', UploadMusic.single('music'), MusicController.uploadMusic);
+router.patch('/api/musics/:id', UploadMusic.single('music'), MusicController.updateMusicById);
+router.delete('/api/musics/:id', MusicController.deleteMusicById);
 
 module.exports = router;
