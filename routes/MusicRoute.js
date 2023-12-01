931a5e6c5cad361105ec31d1d10173d9bb09d842
YUDHA GINONG PRATIDINA <dev.yudhaginongpratidina@gmail.com>
2023-12-01 13:30:43 +0700

    Update

--- routes/MusicRoute.js
+++ routes/MusicRoute.js
@@ -9,9 +9,4 @@ router.post('/api/musics', MusicController.createMusic);
 router.patch('/api/musics/:id', MusicController.updateMusicById);
 router.delete('/api/musics/:id', MusicController.deleteMusicById);
 
-// const UploadMusic = require("../middleware/music");
-// router.post('/api/musics', UploadMusic.single('music'), MusicController.uploadMusic);
-// router.patch('/api/musics/:id', UploadMusic.single('music'), MusicController.updateMusicById);
-// router.delete('/api/musics/:id', MusicController.deleteMusicById);
-
 module.exports = router;
